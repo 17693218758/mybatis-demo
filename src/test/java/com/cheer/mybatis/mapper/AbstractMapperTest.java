10ef50326bfd627442a4bcf210846a46f762a000
17693218758 <56532337@qq.com>
2019-03-18 15:45:35 +0800

    增加导入包注释

--- src/test/java/com/cheer/mybatis/mapper/AbstractMapperTest.java
+++ src/test/java/com/cheer/mybatis/mapper/AbstractMapperTest.java
@@ -1,5 +1,5 @@
 package com.cheer.mybatis.mapper;
-
+//导包
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -7,7 +7,7 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.After;
 import org.junit.Before;
 
-
+import java.io.IOException;
 import java.io.IOException;
 import java.io.InputStream;
 
